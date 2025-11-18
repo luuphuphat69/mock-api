a7a713752cbee7b40549f75964a1057ed1ed5e9e
luuphuphat69 <luuphuphat69@gmail.com>
2025-11-18 22:16:36 +0700

    config next

--- mock-api/next.config.ts
+++ mock-api/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // Required so that NEXT PROXY can receive cookies from other origins
+    serverActions: {
+      allowedOrigins: [
+        "https://mock-api-server-sy5n.onrender.com", // backend
+        "http://localhost:3000",                     // local frontend
+        "https://your-frontend-domain.com",          // prod frontend
+      ],
+    },
+  },
 };
 
 export default nextConfig;
