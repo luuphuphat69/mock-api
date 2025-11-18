0b127b74ba86a323e0c11e2fad3d70d589da5068
luuphuphat69 <luuphuphat69@gmail.com>
2025-11-18 22:09:39 +0700

    fix logout

--- server/controller/authen/logout.js
+++ server/controller/authen/logout.js
@@ -1,5 +1,12 @@
 async function logout(req, res) {
-    res.clearCookie('token', { httpOnly: true, sameSite: 'lax' });
-    return res.status(200).json({ message: 'Logged out' });
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+  });
+
+  return res.status(200).json({ message: "Logged out" });
 }
+
 module.exports = logout;
