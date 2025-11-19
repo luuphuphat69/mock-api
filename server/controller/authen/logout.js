df3458de4ea7206bc944e1df3e4ed10caf4e4c5b
luuphuphat69 <luuphuphat69@gmail.com>
2025-11-19 12:48:10 +0700

    fix logout cookie

--- server/controller/authen/logout.js
+++ server/controller/authen/logout.js
@@ -1,11 +1,11 @@
 async function logout(req, res) {
-  res.clearCookie("token", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  });
-
+res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  domain: ".mockapi.io.vn",
+  path: "/",
+});
   return res.status(200).json({ message: "Logged out" });
 }
 
