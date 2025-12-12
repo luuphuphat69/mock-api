89e0e9aa17d42366ef926f494cf01c7b67374b4c
luuphuphat69 <luuphuphat69@gmail.com>
2025-12-12 23:25:34 +0700

    add: speed insights

--- mock-api/app/layout.tsx
+++ mock-api/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster />
+        <SpeedInsights/>
         {children}
       </body>
     </html>
