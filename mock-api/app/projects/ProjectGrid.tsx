ddea74357c5484546f87f90098bb4dd8a9901851
luuphuphat69 <luuphuphat69@gmail.com>
2025-12-18 13:21:35 +0700

    add: resource grid testid

--- mock-api/app/projects/ProjectGrid.tsx
+++ mock-api/app/projects/ProjectGrid.tsx
@@ -37,6 +37,7 @@ export default function ProjectGrid({
   return (
     <div
       ref={gridRef}
+      data-testid='resource-grid-container'
       className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
     >
       {projects.map((project) => (
