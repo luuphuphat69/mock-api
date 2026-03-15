3a1d926602aa14b8bf9c857ea937491507082026
luuphuphat69 <luuphuphat69@gmail.com>
2026-03-15 23:03:49 +0700

    change config and update api

--- mock-api/next.config.ts
+++ mock-api/next.config.ts
@@ -1,16 +1,32 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     // Required so that NEXT PROXY can receive cookies from other origins
+//     serverActions: {
+//       allowedOrigins: [
+//         "https://api.mockapi.io.vn", 
+//         "https://mock-api-server-sy5n.onrender.com",// backend
+//         "http://localhost:3000",                     // local frontend
+//         "https://mockapi.io.vn",          // prod frontend
+//       ],
+//     },
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // Required so that NEXT PROXY can receive cookies from other origins
-    serverActions: {
-      allowedOrigins: [
-        "https://api.mockapi.io.vn", 
-        "https://mock-api-server-sy5n.onrender.com",// backend
-        "http://localhost:3000",                     // local frontend
-        "https://mockapi.io.vn",          // prod frontend
-      ],
-    },
+  async rewrites() {
+    return [
+      {
+        // This makes /api/login actually call your Render server
+        // but the browser THINKS it is calling your frontend domain
+        source: '/api/:path*',
+        destination: 'https://mock-api-server-sy5n.onrender.com/api/:path*',
+      },
+    ];
   },
 };
 
--- mock-api/utilities/api/api.tsx
+++ mock-api/utilities/api/api.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://mock-api-server-sy5n.onrender.com/api",
-  //baseURL: "http://localhost:8000/api",
+  baseURL: "/api", 
   withCredentials: true,
 });
 
