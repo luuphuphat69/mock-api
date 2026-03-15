07be3b87c8ded6382eb083ad582c834abd48f82c
luuphuphat69 <luuphuphat69@gmail.com>
2026-03-15 22:50:18 +0700

    update config

--- mock-api/next.config.ts
+++ mock-api/next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     // Required so that NEXT PROXY can receive cookies from other origins
     serverActions: {
       allowedOrigins: [
-        "https://api.mockapi.io.vn", // backend
+        "https://api.mockapi.io.vn", 
+        "https://mock-api-server-sy5n.onrender.com",// backend
         "http://localhost:3000",                     // local frontend
         "https://mockapi.io.vn",          // prod frontend
       ],
