9ccec4ad663493f4e7791c699636a519eedc36c8
luuphuphat69 <luuphuphat69@gmail.com>
2026-05-02 22:08:52 +0700

    feat: add UUID generation and manipulation functions
    - Implement SHA1 hashing function for UUID v5 generation.
    - Add stringify and unsafeStringify functions for converting UUIDs to strings.
    - Introduce types for UUID handling, including Version1, Version4, Version5, Version6, and Version7 options.
    - Create v1, v3, v4, v5, v6, and v7 UUID generation functions with support for custom options.
    - Implement conversion functions between UUID v1 and v6.
    - Add validation and version checking for UUIDs.
    - Update package.json for UUID module with necessary dependencies and scripts.

--- test-resources/performance/load-testing/test.js
+++ test-resources/performance/load-testing/test.js
@@ -1,10 +1,62 @@
-import {loadGet, loadPost, loadPatch, loadPut, loadDelete} from '../load-testing/mock/http.js'
+import { loadGet, loadPost, loadPatch, loadPut } from './mock/http.js';
 
+export { loadGet, loadPost, loadPatch, loadPut };
 export const options = {
-  vus: 100,
-  duration: '30m',
-};
+  scenarios: {
+    get_test: {
+      executor: 'constant-vus',
+      vus: 100,
+      duration: '5m',
+      exec: 'loadGet',
+    },
+    post_test: {
+      executor: 'constant-arrival-rate',
+      rate: 60,
+      timeUnit: '1m',
+      duration: '5m',
+      preAllocatedVUs: 5,
+      maxVUs: 20,
+      startTime: '6m',
+      exec: 'loadPost',
+    },
+    put_test: {
+      executor: 'constant-arrival-rate',
+      rate: 60,
+      timeUnit: '1m',
+      duration: '5m',
+      preAllocatedVUs: 5,
+      maxVUs: 20,
+      startTime: '12m',
+      exec: 'loadPut',
+    },
+    patch_test: {
+      executor: 'constant-arrival-rate',
+      rate: 60,
+      timeUnit: '1m',
+      duration: '5m',
+      preAllocatedVUs: 5,
+      maxVUs: 20,
+      startTime: '18m',
+      exec: 'loadPatch',
+    },
+  },
 
-export default function () {
-  loadGet();
-}
+  thresholds: {
+    // 🔹 Scenario-based
+    'http_req_failed{scenario:get_test}': ['rate<0.01'],
+    'http_req_failed{scenario:post_test}': ['rate<0.01'],
+    'http_req_failed{scenario:put_test}': ['rate<0.01'],
+    'http_req_failed{scenario:patch_test}': ['rate<0.01'],
+
+    'http_req_duration{scenario:get_test}': ['p(95)<3000'],
+    'http_req_duration{scenario:post_test}': ['p(95)<3000'],
+    'http_req_duration{scenario:put_test}': ['p(95)<3000'],
+    'http_req_duration{scenario:patch_test}': ['p(95)<3000'],
+
+    // 🔹 Endpoint-based (custom tags)
+    'http_req_failed{endpoint:GET}': ['rate<0.01'],
+    'http_req_failed{endpoint:POST}': ['rate<0.01'],
+    'http_req_failed{endpoint:PUT}': ['rate<0.01'],
+    'http_req_failed{endpoint:PATCH}': ['rate<0.01'],
+  },
+};
